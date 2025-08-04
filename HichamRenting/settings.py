9a1e10530d5a1f83bb577ff709d196562e9b583b
Mohamed Amine <m.amine.karim94@gmail.com>
2025-08-04 22:46:17 +0100

    railway dep

--- HichamRenting/settings.py
+++ HichamRenting/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'HichamRenting.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -97,7 +97,7 @@ DATABASES = {
         'PORT': '5433',
     }
 }
-
+'''
 '''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -108,11 +108,11 @@ DATABASES = {
         'PORT': '5432',
     }
 }'''
-'''
+
 DATABASES = {
     'default': dj_database_url.config()
 }
-'''
+
 print("DATABASE_URL ENV:", os.environ.get("DATABASE_URL"), dj_database_url.config(), 11111)
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
